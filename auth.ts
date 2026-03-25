a6f04cbd436e2d3f28668003ce0b09adccf08099
Becca Sullivan <beccasullivan30@gmail.com>
2026-03-25 13:12:44 +0000

    fix get user sql query

--- auth.ts
+++ auth.ts
@@ -10,7 +10,7 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 async function getUser(email: string): Promise<User | undefined> {
   try {
-    const user = await sql<User[]>`SELECT * FROMusers WHERE email=${email}`;
+    const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
     return user[0];
   } catch (err) {
     console.error("Failed to fetch user:", err);
